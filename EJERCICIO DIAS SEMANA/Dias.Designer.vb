3872b108e1f6e130ed28b3beb814c0ec49f6679c
GonWick <gonxza@gmail.com>
2020-12-22 09:36:06 +0100

    Dias de la Semana

--- EJERCICIO DIAS SEMANA/Dias.Designer.vb
+++ EJERCICIO DIAS SEMANA/Dias.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Dias
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,9 +20,9 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Dias))
         Me.ButtonLunes = New System.Windows.Forms.Button()
         Me.ButtonMartes = New System.Windows.Forms.Button()
         Me.ButtonMiercoles = New System.Windows.Forms.Button()
@@ -104,7 +104,7 @@ Partial Class Form1
         Me.LabelDiaSemana.Size = New System.Drawing.Size(0, 15)
         Me.LabelDiaSemana.TabIndex = 7
         '
-        'Form1
+        'Dias
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
@@ -121,7 +121,8 @@ Partial Class Form1
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
-        Me.Name = "Form1"
+        Me.Name = "Dias"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Dia de la Semana"
         Me.ResumeLayout(False)
         Me.PerformLayout()
--- EJERCICIO DIAS SEMANA/Dias.vb
+++ EJERCICIO DIAS SEMANA/Dias.vb
@@ -1,4 +1,4 @@
-﻿Public Class Form1
+﻿Public Class Dias
     Private Sub ButtonLunes_Click(sender As Object, e As EventArgs) Handles ButtonLunes.Click
         LabelDiaSemana.Text = "Lunes"
     End Sub
